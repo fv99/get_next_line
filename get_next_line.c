b10776161d920044f06b8fbde807290a2c1ad179
fvonsovs <filipvon@protonmail.com>
2023-01-22 21:16:24 +0100

    Added static variable, TODO make it below 25 lines

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 18:02:08 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/01/22 20:45:55 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/01/22 21:16:14 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,18 @@ int	get_char(int fd)
 }
 
 // TODO:
-// recode this to use a static variable to keep track
-// of the file descriptor in between function calls
-// 
-// store the number of bytes read in previous call
-// use this value to continue reading on next call
+// recode this to less than 25 lines
 
 char	*get_next_line(int fd)
 {
-	int		i;
-	char	c;
-	char	*buf;
+	static int	bytes;
+	int			i;
+	char		c;
+	char		*buf;
 
+	bytes = 0;
 	i = 0;
-	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	buf = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
 	buf[i] = '\0';
@@ -53,34 +51,42 @@ char	*get_next_line(int fd)
 		if (c == '\n')
 		{
 			buf[i++] = c;
-			break ;
+			buf[i] = '\0';
+			bytes += i;
+			return (buf);
 		}
 		buf[i++] = c;
 		c = get_char(fd);
 	}
 	buf[i] = '\0';
-	if (c == -1 && i == 0)
-		return (NULL);
-	return (buf);
+	if (c == -1 && i > 0)
+	{
+		bytes += i;
+		return (buf);
+	}
+	return (NULL);
 }
 
-int	main(int argc, char* argv[]) 
+int	main(int argc, char *argv[])
 {
-	char* line;
-	if (argc < 2) {
+	char	*line;
+	int		fd;
+
+	if (argc < 2)
+	{
 		printf("Usage: %s <file>\n", argv[0]);
-		return 1;
+		return (1);
 	}
-	int fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		perror("Failed to open file");
-		return 1;
+		return (1);
 	}
-	while ((line = get_next_line(fd)) != NULL)
+	while (line != NULL)
 	{
+		line = get_next_line(fd);
 		printf("%s", line);
-		free(line);
 	}
 	close (fd);
 	return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 18:02:28 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/01/22 18:04:06 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/01/22 21:14:25 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 255
 # endif
 
 char	*get_next_line(int fd);
