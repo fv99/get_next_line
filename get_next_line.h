1bdaca25faa54b83503981ff5fb4f896fb2e2c9b
fvonsovs <filipvon@protonmail.com>
2023-01-22 18:27:30 +0100

    Added get_next_line.c + header file

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 18:02:28 by fvonsovs          #+#    #+#             */
+/*   Updated: 2023/01/22 18:04:06 by fvonsovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
